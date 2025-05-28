b9fc64b7742a0046b0ad97ad0eb577bf9ef4f3e2
Kartik Gandhi <kartik.gandhi@tothenew.com>
2025-05-28 16:36:17 +0530

    improve css and add dynamic status

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,11 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +13,19 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked!');
+    console.log(this.el);
+  }
 }
